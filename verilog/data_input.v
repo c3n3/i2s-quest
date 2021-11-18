dfbf42f752370c03e2b3b263a7c8fdc8149d42ce
grboehringer <g2boehringer@gmail.com>
2021-11-18 15:45:05 -0600

    verilog

--- verilog/data_input.v
+++ verilog/data_input.v
@@ -1,39 +1,31 @@
 module data_input(
 	input clk, 
 	input serial, 
-	input ready, 
-	output rpi_interrupt,
-	output [23:0] data
+	input ready,
+	output reg [23:0] data
 	);
 	
 	reg [5:0] reg_selector;
+	reg [5:0] curr_reg;
 	reg [4:0] counter;
 	reg state;
-	reg [23:0] data_reg [63:0];
+	reg [63:0] data_regs [23:0];
 	
 	always@(posedge clk)
 	begin
-		if (~state)
+		data_regs[reg_selector][counter] = serial;
+		counter = counter + 1;
+		if (counter > 23)
 		begin
-			data_reg[selector] = serial;
-			selector = selector + 1;
-			if (selector > 23)
-			begin
-				state = ~state;
-				selector = 0;
-				rpi_interrupt = 0;
-			end
+			counter = 0;
+			reg_selector = reg_selector + 1;
 		end
 	end
 	
 	always@(posedge ready)
 	begin
-		if (state)
-		begin
-			data = data_reg;
-			state = ~state;
-			rpi_interrupt = 1;
-		end
+		data = data_regs[curr_reg];
+		curr_reg = curr_reg + 1;
 	end
 	
 endmodule
--- verilog/data_shift.v
+++ verilog/data_shift.v
@@ -1,14 +1,21 @@
 module data_shift(
 	input clk,
+	input [23:0] data,
+	output reg ready,
 	output reg current);
-	reg [23:0] data;
+	
+	reg [23:0] data_reg;
 	reg [4:0] selector;
 	
 	always@(posedge clk)
 	begin
-		data = 24'd4000000;
-		current = data[selector];
+		if (selector == 0) data_reg = data;
+		current = data_reg[selector];
 		selector = selector + 1;
+		
+		if (selector == 23) ready = 1;
+		else ready = 0;
+		
 		if (selector > 23)
 		begin
 			selector = 0;
